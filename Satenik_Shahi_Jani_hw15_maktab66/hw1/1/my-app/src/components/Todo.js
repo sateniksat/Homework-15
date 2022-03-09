3d089966fb2cf276329c9598452b8ad2bf128d11
Satenik Shahi <shahijanisaten@gmail.com>
2022-03-09 22:22:18 +0330

    add

--- Satenik_Shahi_Jani_hw15_maktab66/hw1/1/my-app/src/components/Todo.js
+++ Satenik_Shahi_Jani_hw15_maktab66/hw1/1/my-app/src/components/Todo.js
@@ -3,6 +3,8 @@ import TodoForm from './TodoForm';
 // import { RiEdit2Line } from "react-icons/fa";
 // import { RiCloseFill } from "react-icons/fa";
 // import { AiOutlineCloseCircle } from "react-icons/fa";
+// import { FiEdit } from "react-icons/fa";
+import { FaRegTimesCircle,FaPen } from "react-icons/fa";
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
@@ -31,10 +33,13 @@ const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
         {todo.text}
       </div>
       <div className='icons'>
-        <div style={{padding:"2px",border:"white 1px solid"}} onClick={() => removeTodo(todo.id)}>x </div><div  onClick={() => setEdit({ id: todo.id, value: todo.text })}>edit</div>
+        {/* <div style={{padding:"2px",border:"white 1px solid"}} onClick={() => removeTodo(todo.id)}>x </div><div  onClick={() => setEdit({ id: todo.id, value: todo.text })}>edit</div> */}
         {/* <RiEdit2Line/> */}
         {/* <RiCloseFill/> */}
         {/* <AiOutlineCloseCircle/> */}
+        {/* <FiEdit/> */}
+        <FaPen style={{marginRight:"5%"}} onClick={() => setEdit({ id: todo.id, value: todo.text })} />
+        <FaRegTimesCircle onClick={() => removeTodo(todo.id)}/>
       </div>
     </div>
   ));
