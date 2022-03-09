2dea7fb3c357d365788e3bf915b7b5e527635f2b
Satenik Shahi <shahijanisaten@gmail.com>
2022-03-09 22:05:17 +0330

    add

---
+++ Satenik_Shahi_Jani_hw15_maktab66/hw1/1/src/components/Todo.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+// import { RiEdit2Line } from "react-icons/fa";
+// import { RiCloseFill } from "react-icons/fa";
+// import { AiOutlineCloseCircle } from "react-icons/fa";
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <div style={{padding:"2px",border:"white 1px solid"}} onClick={() => removeTodo(todo.id)}>x </div><div  onClick={() => setEdit({ id: todo.id, value: todo.text })}>edit</div>
+        {/* <RiEdit2Line/> */}
+        {/* <RiCloseFill/> */}
+        {/* <AiOutlineCloseCircle/> */}
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
